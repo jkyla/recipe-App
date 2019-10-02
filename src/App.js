a57e00714a21b1e84c28d32f582bbb788c98acc1
Kyla Johnson <johnsonkyla18@yahoo.com>
2019-10-02 14:27:30 -0500

    used recipe api to pull data when the user inputs a search
    including images, ingredients, calorie count

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, {useEffect, useState} from 'react';
+import Recipe from "./Recipe";
 import './App.css';
 
 const App = () => {
-  const APP_ID = "002dae7b";
-  const APP_KEY = "7512c38d7240954f3c473dffe2332602";
+  const APP_ID = "d2aa66b7";
+  const APP_KEY = "97d018913c2d6ce1d99840d6946e4b82";
 
-const exampleReq = 'https://api.edamam.com/search?q=chicken&app_id=${002dae}&app_key=${7512c38d7240954f3c473dffe2332602}'
- 
-const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
- useEffect(() => {
-  console.log("Effect has been run");
- });
+ useEffect( () => {
+  getRecipes();
+ }, [query]);
  
+ const getRecipes = async () => {
+  const response = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  setRecipes(data.hits);
+  console.log(data.hits);
+};
  
- 
+const updateSearch =  e => {
+  setSearch(e.target.value);
+  console.log(search); 
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
+
   return (
     <div className = "App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input 
+          className="search-bar" 
+          type="text" 
+          value={search} 
+          onChange={updateSearch} 
+          />
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick ={() => setCounter(counter + 1)}>{counter}</h1>
+      
+    <div className= "recipes">
+        {recipes.map(recipe => (
+          <Recipe 
+            key={recipe.recipe.label}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
     </div>
+   </div>
   );
 };
 
